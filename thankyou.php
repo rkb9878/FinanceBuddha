9318af2c6667e243862ba0ecf595c83414fed0b8
Rahul <rkb9878@gmail.com>
2020-09-01 12:54:40 +0530

    change in footer

--- thankyou.php
+++ thankyou.php
@@ -6,19 +6,236 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thankyou</title>
-    <?php include 'headerfiles.php'?>
+    <?php include 'headerfiles.php' ?>
 </head>
 <body>
-<?php include 'navbar.php'?>
+<?php include 'navbar.php' ?>
 <section>
     <div class="text-center" style="margin-top: 130px; margin-bottom: 150px">
-        <h1 class="display-4">Thank you! <?php echo $_GET['d']?></h1>
+        <h1 class="display-4">Thank you! <?php echo $_GET['d'] ?></h1>
         <div>
             <span class="text-success"><i class="fa-4x fa fa-check-circle"></i></span>
         </div>
     </div>
 </section>
 
-<?php include 'footer.php'?>
+
+<footer style=" background-color: black;  text-align: center;  padding: 10px;">
+
+
+    <div class="container-fluid pl-5 pr-5">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="text-left pb-4">
+                    <img src="images/bank/header-logo.png" alt="">
+                </div>
+                <p class="text-white collapse-p" style="font-size: 12px">
+                    We are not yet another online offer comparison site. While we do
+                    give you the most exhaustive range of options, we do the last mile
+                    and get you the loan also. We strongly believe that financial
+                    services is not like an ecommerce <a href="about-us.php">Read More >></a>
+                </p>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-left text-center ">
+                    <h6 class="text-white " style="text-decoration: underline">Quick Link</h6>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <ul style="list-style-type: none;text-align: left;color: white">
+                            <li><a style="font-size: 12px" class="text-white" href="index.php">Home</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="home-loan.php">Home Loan</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="Personal-loan.php">Personal
+                                    Loan</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="business-loan.php">Business
+                                    Loan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <ul style="list-style-type: none;text-align: left;color: white">
+                            <li><a style="font-size: 12px" class="text-white" href="about-us.php">About Us</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="contact-us.php">Contact Us</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="term-and-condition.php">Terms of
+                                    Use</a></li>
+                            <li><a style="font-size: 12px" class="text-white" href="privacy-policy.php">privacy
+                                    policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="pt-5">
+                    <div class="text-center">
+                        <h4 class="text-white font-weight-bold">Follow Us</h4>
+                    </div>
+                    <br>
+                    <div class="social-icon">
+                        <div class=" text-center">
+                            <a href="#"><i class="fa-2x fab fa-facebook"></i></a>
+                        </div>
+                        <div class=" text-center">
+                            <a href="#"><i class="fa-2x fab fa-linkedin"></i></a>
+                        </div>
+                        <div class=" text-center">
+                            <a href="#"><i class="fa-2x fab fa-twitter"></i></a>
+                        </div>
+                        <div class=" text-center">
+                            <a href="#"><i class="fa-2x fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-white">
+        <p style="color: white">Copyright © 2020 Finance Buddha</p>
+    </div>
+</footer>
+
+
+<div id="myModal" class="modal fade in " aria-hidden="false">
+    <div class="modal-dialog mm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title" style="text-align:center">Loan Apply Form</h2>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>
+            <div class="modal-body" style="padding: 2rem">
+                <!--Body-->
+                <!-- start contact-pg-content -->
+                <section class="shop-checkout">
+                    <div class="">
+                        <div class="row">
+                            <div class="col col-md-12">
+                                <div class="contact-form p-3">
+                                    <form method="post" action="emailsend/loanApplyForm.php" class="row custom-form">
+                                        <div class="row">
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Name of Applicant</label>
+                                                <input type="text" id="Form-email5" name="name"
+                                                       placeholder="Enter Applicant Name"
+                                                       class="modalInput form-control validate white-text"
+                                                       required>
+                                            </div>
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Mobile</label>
+                                                <input type="number" id="Form-email5" name="mobile"
+                                                       placeholder="Enter Mobile No."
+                                                       class=" modalInput form-control validate white-text"
+                                                       required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Email ID</label>
+                                                <input type="email" id="Form-email5" name="email"
+                                                       placeholder="Enter Email ID"
+                                                       class="modalInput form-control validate white-text" required>
+                                            </div>
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>State</label>
+                                                <select id="Form-email5" class="modalInput" name="state" required>
+                                                    <option value="0">-Select State-</option>
+                                                    <option value="Andaman and Nicobar Islands">Andaman and Nicobar
+                                                        Islands
+                                                    </option>
+                                                    <option value="Andhra Pradesh">Andhra Pradesh</option>
+                                                    <option value="Assam">Assam</option>
+                                                    <option value="Chandigarh">Chandigarh</option>
+                                                    <option value="Dadra and Nagar Haveli">Dadra and Nagar Haveli
+                                                    </option>
+                                                    <option value="Delhi">Delhi</option>
+                                                    <option value="Gujarat">Gujarat</option>
+                                                    <option value="Haryana">Haryana</option>
+                                                    <option value="Himachal Pradesh">Himachal Pradesh</option>
+                                                    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+                                                    <option value="Kerala">Kerala</option>
+                                                    <option value="Lakshadweep">Lakshadweep</option>
+                                                    <option value="Maharashtra">Maharashtra</option>
+                                                    <option value="Manipur">Manipur</option>
+                                                    <option value="Meghalaya">Meghalaya</option>
+                                                    <option value="Karnataka">Karnataka</option>
+                                                    <option value="Nagaland">Nagaland</option>
+                                                    <option value="Orissa">Orissa</option>
+                                                    <option value="Puducherry">Puducherry</option>
+                                                    <option value="Punjab">Punjab</option>
+                                                    <option value="Rajasthan">Rajasthan</option>
+                                                    <option value="Tamil Nadu">Tamil Nadu</option>
+                                                    <option value="Tripura">Tripura</option>
+                                                    <option value="West Bengal">West Bengal</option>
+                                                    <option value="Sikkim">Sikkim</option>
+                                                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                                                    <option value="Mizoram">Mizoram</option>
+                                                    <option value="Daman and Diu">Daman and Diu</option>
+                                                    <option value="Goa">Goa</option>
+                                                    <option value="Bihar">Bihar</option>
+                                                    <option value="Madhya Pradesh">Madhya Pradesh</option>
+                                                    <option value="Uttar Pradesh">Uttar Pradesh</option>
+                                                    <option value="Chhattisgarh">Chhattisgarh</option>
+                                                    <option value="Jharkhand">Jharkhand</option>
+                                                    <option value="Uttarakhand">Uttarakhand</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>City Name</label>
+                                                <input type="text" name="city" id="Form-email5"
+                                                       placeholder="Enter City Name"
+                                                       class="modalInput form-control validate white-text"
+                                                       required>
+                                            </div>
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Pincode</label>
+                                                <input type="number" name="pincode" id="Form-email5"
+                                                       placeholder="Enter Pincode"
+                                                       class="modalInput form-control validate white-text"
+                                                       required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Loan Type</label>
+                                                <select name="loan_type" class="modalInput select required"
+                                                        title="Name (Required)">
+                                                    <option>-Select Loan Type-</option>
+
+                                                    <option>Personal Loan</option>
+                                                    <option>Business Loan</option>
+
+                                                </select>
+                                            </div>
+                                            <div class="col-xs-12 col-md-6">
+                                                <label>Loan Amount</label>
+                                                <input type="number" name="amt" id="Form-email5"
+                                                       placeholder="Enter Loan Amount"
+                                                       class="modalInput white-text"
+                                                       required>
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <input type="submit" value="Submit" name="submit"
+                                                   class="btn " style="background: blue;color: whitesmoke">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div> <!-- end row -->
+                    </div> <!-- end container -->
+                </section>
+                <!-- end contact-pg-content -->
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="bootstrap-4.5.1-dist/js/jquery-3.4.1.js"></script>
+<script src="bootstrap-4.5.1-dist/js/popper.min.js"></script>
+<script src="bootstrap-4.5.1-dist/js/bootstrap.js"></script>
+<script src="fontawesome/js/all.js"></script>
+<script src="https://kit.fontawesome.com/bdd89edb33.js"></script>
+
+
 </body>
 </html>
